2b5af40ba89df447bb76dbf2cdce58f06d39445c
Z_Doctor <13635485+Z-Doctor@users.noreply.github.com>
2019-08-11 16:01:59 -0700

    Revert "Added SkillApi back into master"
    This reverts commit f0b3d99ad1113d52131667ac2af238341a51adcf.

--- src/main/java/zdoctor/skilltree/api/package-info.java
+++
@@ -1,4 +0,0 @@
-@API(owner = "skilltree", apiVersion = "1.0.0.0", provides = "skilltree|API")
-package zdoctor.skilltree.api;
-
-import net.minecraftforge.fml.common.API;
--- src/main/java/zdoctor/skilltree/api/skills/interfaces/ISkillSellable.java
+++
@@ -1,7 +0,0 @@
-package zdoctor.skilltree.api.skills.interfaces;
-
-import net.minecraft.entity.EntityLivingBase;
-
-public interface ISkillSellable {
-	public void onSold(EntityLivingBase owner);
-}
